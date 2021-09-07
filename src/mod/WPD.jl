ffaa21576005b2fbe51e75e2a34f1d3d39849db6
Zeng Fung Liew <zengfung.liew@gmail.com>
2021-09-06 19:38:37 -0700

    Added support of wavelet transforms for multiple signals

--- src/mod/WPD.jl
+++ src/mod/WPD.jl
@@ -60,10 +60,13 @@ function wpd!(y::AbstractArray{T,2}, x::AbstractArray{T,1},
     return nothing
 end
 
-function wpd!(y::AbstractArray{T,2}, x::AbstractArray{T,1}, 
-        wt::DiscreteWavelet, hqf::Array{S,1}, gqf::Array{S,1}, 
-        L::Integer=maxtransformlevels(x)) where {T<:Number, S<:Number}
-
+function wpd!(y::AbstractArray{T,2}, 
+              x::AbstractArray{T,1}, 
+              wt::DiscreteWavelet, 
+              hqf::Array{S,1}, 
+              gqf::Array{S,1}, 
+              L::Integer=maxtransformlevels(x)) where 
+             {T<:Number, S<:Number}
     n = length(x)
     @assert 0 <= L <= maxtransformlevels(n)
     @assert size(y) == (n, L+1)
@@ -88,6 +91,146 @@ function wpd!(y::AbstractArray{T,2}, x::AbstractArray{T,1},
         end
     end
     return nothing
+end
+
+# ========== Transforms On A Group Of Signals ==========
+"""
+    dwtall(x, wt[, L])
+
+Computes the discrete wavelet transform (DWT) on each slice of signal. Signals are sliced on
+the ``n``-th dimension for an ``n``-dimensional input `x`.
+
+*Note:* `dwt` is currently available for 1-D, 2-D, and 3-D signals only. 
+
+# Arguments
+- `x::AbstractArray{T} where T<:Number`: Input signals, where each slice corresponds to
+  one signal. For a set of input signals `x` of dimension ``n``, signals are sliced on the
+  ``n``-th dimension.
+- `wt::OrthoFilter`: Wavelet used.
+- `L::Integer`: (Default: `Wavelets.maxtransformlevels(xᵢ)`) Number of levels of wavelet
+  transforms. 
+
+# Returns
+`::Array{T}`: Slices of transformed signals. Signals are sliced the same way as the input
+signal `x`.
+
+# Examples
+```
+using Wavelets, WaveletsExt
+
+# Generate random signals
+x = randn(32, 5)
+# Create wavelet
+wt = wavelet(WT.db4)
+
+# DWT on all signals in x
+xw = dwtall(x, wt)
+```
+"""
+function dwtall(x::AbstractArray{T}, args...) where T<:Number
+    # Sanity check
+    @assert ndims(x) > 1
+
+    # Allocate space for transforms
+    y = similar(x)
+    # Dimension to slice
+    dim = ndims(y)
+    # Compute transforms
+    @views for (yᵢ, xᵢ) in zip(eachslice(y, dims=dim), eachslice(x, dims=dim))
+        dwt!(yᵢ, xᵢ, args...)
+    end
+    return y
+end
+
+"""
+    wptall(x, wt)
+
+    wptall(x, wt, L)
+
+    wptall(x, wt, tree)
+
+Computes the wavelet packet transform (WPT) on each slice of signal. Signals are sliced on
+the ``n``-th dimension for an ``n``-dimensional input `x`.
+
+*Note:* `wpt` is currently available for 1-D signals only.
+    
+# Arguments
+- `x::AbstractArray{T} where T<:Number`: Input signals, where each slice corresponds to
+  one signal. For a set of input signals `x` of dimension ``n``, signals are sliced on the
+  ``n``-th dimension.
+- `wt::OrthoFilter`: Wavelet used.
+- `L::Integer`: (Default: `Wavelets.maxtransformlevels(xᵢ)`) Number of levels of wavelet
+  decomposition. 
+- `tree::BitVector`: (Default: `Wavelets.maketree(xᵢ, :full)`) Tree to follow for wavelet
+  decomposition. 
+
+# Returns
+`::Array{T}`: Slices of transformed signals. Signals are sliced the same way as the input
+signal `x`.
+
+# Examples
+```
+using Wavelets, WaveletsExt
+
+# Generate random signals
+x = randn(32, 5)
+# Create wavelet
+wt = wavelet(WT.db4)
+
+# WPT on all signals in x
+xw = wptall(x, wt)
+```
+"""
+function wptall(x::AbstractArray{T}, args...) where T<:Number
+    # Sanity check
+    @assert ndims(x) > 1
+    
+    # Allocate space for transform
+    y = similar(x)
+    # Dimension to slice
+    dim = ndims(x)
+    # Compute transforms
+    @views for (yᵢ, xᵢ) in zip(eachslice(y, dims=dim), eachslice(x, dims=dim))
+        wpt!(yᵢ, Array(xᵢ), args...)
+    end
+    return y
+end
+
+"""
+    wpdall(x, wt[, L])
+
+    wpdall(y, x, wt, hqf, gqf[, L])
+
+Computes the wavelet packet decomposition (WPD) on each slice of signal. Signals are sliced
+on the ``n``-th dimension for an ``n``-dimensional input `x`.
+
+*Note:* `wpd` is currently available for 1-D signals only.
+
+# Arguments
+- `x::AbstractArray{T} where T<:Number`: Input signals, where each slice corresponds to
+  one signal. For a set of input signals `x` of dimension ``n``, signals are sliced on the
+  ``n``-th dimension.
+- `wt::OrthoFilter`: Wavelet used.
+- `L::Integer`: (Default: `Wavelets.maxtransformlevels(xᵢ)`) Number of levels of wavelet
+  decomposition. 
+
+# Returns
+`::Array{T}`
+"""
+function wpdall(x::AbstractArray{T}, args...) where T<:Number
+    # TODO: Not the most optimal solution. Using list comprehension + concatenating the list
+    # TODO: of arrays means array allocation is done on the fly, ie. more arrays to
+    # TODO: decompose = more allocations = more time wasted. Doing concatenation after that
+    # TODO: further wastes more time.
+    # Sanity check
+    @assert ndims(x) > 1
+        
+    # Dimension to slice
+    dim = ndims(x)
+    # Compute transforms
+    y = [wpd(xᵢ, args...) for xᵢ in eachslice(x, dims=dim)] |>
+        xw -> cat(xw..., dims=dim+1)
+    return y
 end
 
 end # end module
