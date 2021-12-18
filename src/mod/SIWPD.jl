d72aaf35723984a2ec9d7c01e286126739942c43
Zeng Fung Liew <zengfung.liew@gmail.com>
2021-12-17 23:54:05 -0800

    Speed up SIWPD functions
    *Note: This is a rewrite of the previous commit message due to some mistakes in the previous
    commit.*
    
    Sped up SIWPD functions using similar structure as DWT and SWT modules. Given a length 32
    signal and a Daubechies 4-tap wavelet, the previous functions' benchmark is:
    ```julia
    julia> @benchmark siwpd(x, wt)
    BenchmarkTools.Trial: 10000 samples with 1 evaluation.
     Range (min … max):  359.400 μs …  11.286 ms  ┊ GC (min … max): 0.00% … 95.35%
     Time  (median):     395.050 μs               ┊ GC (median):    0.00%
     Time  (mean ± σ):   442.743 μs ± 453.846 μs  ┊ GC (mean ± σ):  5.49% ±  5.16%
    
      █▆
      ██▅▃▇▅▃▂▂▃▂▆▆▃▂▂▂▃▂▂▂▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁ ▂
      359 μs           Histogram: frequency by time          843 μs <
    
     Memory estimate: 260.39 KiB, allocs estimate: 2670.
    ```
    
    The new functions' benchmark is:
    ```julia
    julia> @benchmark siwpd(x, wt)
    BenchmarkTools.Trial: 10000 samples with 1 evaluation.
     Range (min … max):  259.800 μs …   8.461 ms  ┊ GC (min … max): 0.00% … 94.63%
     Time  (median):     277.400 μs               ┊ GC (median):    0.00%
     Time  (mean ± σ):   325.950 μs ± 402.596 μs  ┊ GC (mean ± σ):  7.72% ±  6.03%
    
      █▆▃▆▅▄▄▃▃▂▃▃▁▁ ▃▄▂▁▁▂▂▁                                       ▂
      ███████████████████████████▇▇▇▆▆▅▆▄▅▅▅▁▄▁▅▅▅▄▄▄▃▁▄▄▃▄▅▄▄▄▄▃▁▄ █
      260 μs        Histogram: log(frequency) by time        680 μs <
    
     Memory estimate: 355.31 KiB, allocs estimate: 4097.
    ```
    
    It can be seen that the new function is slightly faster but requires significantly more
    allocations. However, considering the number of mathematical operations should be the same
    as/less than that of the Stationary Wavelet Transform (SWT), our ultimate goal should be:
    ```julia
    julia> @benchmark swpd(x, wt)
    BenchmarkTools.Trial: 10000 samples with 1 evaluation.
     Range (min … max):  49.300 μs …  5.679 ms  ┊ GC (min … max): 0.00% … 98.87%
     Time  (median):     54.900 μs              ┊ GC (median):    0.00%
     Time  (mean ± σ):   58.746 μs ± 78.500 μs  ┊ GC (mean ± σ):  1.87% ±  1.40%
    
      ▁▅▃▅▃▅█▆▆▅▄▄▄▃▁▂▁▂▂▂▁▁                                      ▂
      ████████████████████████▇██████▇▇▇▇▇▆▆▆▆▅▅▅▅▆▆▆▇▆▆▆▆▆▆▆▆▄▅▅ █
      49.3 μs      Histogram: log(frequency) by time      95.7 μs <
    
     Memory estimate: 23.88 KiB, allocs estimate: 96.
    ```
    
    The current issue with the new function is mainly the number of allocations involved. In
    fact, to achieve the true purpose of SIWPD as per Cohen's paper, there may be a need to
    consider a different data structure.

--- src/mod/SIWPD.jl
+++ src/mod/SIWPD.jl
@@ -18,94 +18,102 @@ by Cohen, Raz & Malah on the vector `x` using the discrete wavelet filter `wt`
 for `L` levels with depth `d`.
 """
 function siwpd(x::AbstractVector{T}, wt::OrthoFilter, 
-        L::Integer=maxtransformlevels(x), d::Integer=L) where T<:Number
-    
-    g, h = WT.makereverseqmfpair(wt, true)       
-    si = Vector{T}(undef, length(wt)-1)                 # tmp filter vector              
-    return siwpd(x, wt, h, g, si, L, d)
+               L::S = maxtransformlevels(x), d::S = L) where {T<:AbstractFloat, S<:Integer}
+    # Sanity check
+    n = length(x)
+    @assert 0 ≤ L ≤ maxtransformlevels(n)
+    @assert 1 ≤ d ≤ L
+
+    # Setup
+    g, h = WT.makereverseqmfpair(wt, true)
+    # y = Matrix{T}(undef, (n, gettreelength(1<<(L+1))))
+    y = zeros(T, (n, gettreelength(1<<(L+1))))
+    @inbounds y[:,1] = x
+
+    # Decomposition
+    for i in axes(y,2)
+        lvl = getdepth(i, :binary)
+        len = nodelength(n, lvl)
+        dₗ = (0 ≤ lvl ≤ L-d) ? d : L-lvl
+        siwpd_subtree!(y, h, g, i, dₗ, 0, L=lvl, len=len)
+    end
+    return y
 end
 
-function siwpd(x::AbstractVector{Tx}, wt::OrthoFilter, h::Vector{T}, 
-        g::Vector{T}, si::Vector{T}, L::Integer, d::Integer=L) where 
-        {Tx<:Number, T<:Number}
+#=======================================================================================
+Recursive calls for decomposition while d>0.
+If shift is 0, ie. ss=0, the main root will not be further decomposed even if d>0.
+=======================================================================================#
+function siwpd_subtree!(y::AbstractMatrix{T₁}, h::Vector{T₂}, g::Vector{T₂}, 
+                        i::S, d::S, ss::S; 
+                        L::S = getdepth(i, :binary),
+                        len::S = nodelength(size(y,1), L)) where 
+                       {T₁<:AbstractFloat, T₂<:AbstractFloat, S<:Integer}
+    # Sanity check
+    n, k = size(y)
+    @assert 0 ≤ L ≤ maxtransformlevels(n)
+    @assert 0 ≤ ss < n
+    @assert 0 ≤ d ≤ getdepth(k,:binary)-L
+    @assert iseven(len) || len == 1
 
-    n = length(x)
-    @assert isdyadic(n)
-    @assert 0 <= L <= maxtransformlevels(n)
-    @assert 1 <= d <= L
+    # --- Base case ---
+    d > 0 || return y
 
-    # decomposed array y 
-    y = Array{T,2}(undef, (n,2^(L+1)-1))
-    y[:,1] = x
-
-    for node in axes(y,2)
-        level = floor(Int, log2(node)) # current node level (starts from 0)
-        nodelen = nodelength(n, level)
-        # decomposition depth of current node
-        dₗ = (0 <= level <= L-d) ? d : L-level  
-        decompose!(y, wt, h, g, si, node, nodelen, 
-            level, dₗ, 0, zeroshiftdecomposition=false)
+    # --- One level of decomposition ---
+    v_start = ss*len+1                                      # Start index for parent node
+    v_end = (ss+1)*len                                      # End index for parent node
+    @inbounds v = @view y[v_start:v_end, i]                 # Parent node
+    w_start = ss*(len÷2)+1                                  # Start index for child node
+    w_end = w_start+(len÷2)-1                               # End index for child node
+    @inbounds w₁ = @view y[w_start:w_end, getchildindex(i, :left)]    # Left child node
+    @inbounds w₂ = @view y[w_start:w_end, getchildindex(i, :right)]   # Right child node
+    sidwt_step!(w₁, w₂, v, h, g, false)                          # 1 step of non-shifted decomposition
+    w_start = (ss+1<<L)*(len÷2)+1                           # Start index for shifted child node
+    w_end = w_start+(len÷2)-1                               # End index for shifted child node
+    @inbounds w₁ = @view y[w_start:w_end, getchildindex(i, :left)]    # Left child node
+    @inbounds w₂ = @view y[w_start:w_end, getchildindex(i, :right)]   # Right child node
+    sidwt_step!(w₁, w₂, v, h, g, true)                           # 1 step of shifted decomposition
+    
+    # --- Recursive decomposition while d>0 ---
+    if ss > 0       # Decomposition of current shift
+        siwpd_subtree!(y, h, g, getchildindex(i, :left), d-1, ss, L=L+1, len=len÷2)
+        siwpd_subtree!(y, h, g, getchildindex(i, :right), d-1, ss, L=L+1, len=len÷2)
     end
+    # Decomposition of shifted version
+    siwpd_subtree!(y, h, g, getchildindex(i, :left), d-1, ss+1<<L, L=L+1, len=len÷2)
+    siwpd_subtree!(y, h, g, getchildindex(i, :right), d-1, ss+1<<L, L=L+1, len=len÷2)
 
     return y
 end
 
-function decompose!(y::AbstractArray{Ty,2}, wt::OrthoFilter, 
-        h::Vector{T}, g::Vector{T}, si::Vector{T}, 
-        node::Integer, nodelen::Integer, level::Integer, d::Integer, 
-        shift::Integer; zeroshiftdecomposition::Bool=true) where 
-        {Ty<:Number, T<:Number}
+# Single step of decomposition
+function sidwt_step!(w₁::AbstractVector{T}, w₂::AbstractVector{T},
+                v::AbstractVector{T},
+                h::Vector{S}, g::Vector{S},
+                s::Bool) where {T<:AbstractFloat, S<:AbstractFloat}
+    # Sanity check
+    @assert length(w₁) == length(w₂) == length(v)÷2
+    @assert length(h) == length(g)
 
-    # base case
-    @assert d > 0 || return nothing
-    @assert 0 <= level <= maxtransformlevels(size(y,1))           
-    @assert d <= maxtransformlevels(size(y,2)+1) - level        
-    @assert 0 <= shift < size(y,1)                                                       
-    filtlen = length(wt)
-    
-    pstart = shift*nodelen + 1                                  # start index
-    pend = (shift+1)*nodelen                                    # end index
-    parent = y[pstart:pend, node]
+    # Setup
+    n = length(v)           # Parent length
+    n₁ = length(w₁)         # Child length
+    filtlen = length(h)     # Filter length
 
-    cnodelen = nodelen ÷ 2
-    cstart = shift*cnodelen + 1
-    lchild = @view y[:, getchildindex(node,:left)]
-    rchild = @view y[:, getchildindex(node,:right)]
-
-    # non-shifted subtree
-    Wavelets.Transforms.filtdown!(      # scaling coefficients
-        g, si, lchild, cstart, cnodelen, parent, 1, 0, false
-    )   
-    Wavelets.Transforms.filtdown!(      # detail coefficients
-        h, si, rchild, cstart, cnodelen, parent, 1, -filtlen+1, true
-    )   
-    if zeroshiftdecomposition   # pursue the same thing with d-1 if shift > 0
-        decompose!(                     # decompose left child
-            y, wt, h, g, si, getchildindex(node,:left), cnodelen, level+1, d-1, shift
-        )   
-        decompose!(                     # decompose right child
-            y, wt, h, g, si, getchildindex(node,:right), cnodelen, level+1, d-1, shift
-        )   
+    # One step of discrete transform
+    for i in 1:n₁
+        k₁ = mod1(2*i-1-s, n)   # Start index for low pass filtering
+        k₂ = 2*i-s              # Start index for high pass filtering
+        @inbounds w₁[i] = g[end] * v[k₁]
+        @inbounds w₂[i] = h[1] * v[k₂]
+        for j in 2:filtlen
+            k₁ = k₁+1 |> k₁ -> k₁>n ? mod1(k₁,n) : k₁
+            k₂ = k₂-1 |> k₂ -> k₂≤0 ? mod1(k₂,n) : k₂
+            @inbounds w₁[i] += g[end-j+1] * v[k₁]
+            @inbounds w₂[i] += h[j] * v[k₂]
+        end
     end
-
-    # shifted subtree
-    parent = circshift(parent,1)
-    cstart = (shift + 1<<level)*cnodelen + 1
-    Wavelets.Transforms.filtdown!(      # scaling coefficients
-        g, si, lchild, cstart, cnodelen, parent, 1, 0, false
-    )
-    Wavelets.Transforms.filtdown!(      # detail coefficients
-        h, si, rchild, cstart, cnodelen, parent, 1, -filtlen+1, true
-    )
-    # pursue the same thing with d-1
-    decompose!(                         # decompose shifted left child
-        y, wt, h, g, si, getchildindex(node,:left), cnodelen, level+1, d-1, shift+1<<level
-    )       
-    decompose!(                         # decomposed shifted right child
-        y, wt, h, g, si, getchildindex(node,:right), cnodelen, level+1, d-1, shift+1<<level
-    )     
-
-    return nothing
+    return w₁, w₂
 end
 
 """
